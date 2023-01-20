f1012a02c3e3c81987dde0318ef618f959a9a244
Stevixent <chiadikavirtue@gmail.com>
2023-01-20 17:35:13 +0100

    oo

--- main.js
+++ main.js
@@ -2,4 +2,10 @@ window.addEventListener('load', () => {
     const form = document.querySelector("todo-list");
     const input = document.querySelector("todo-input");
     const list_el = document.querySelector("#task");
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const task = input.value;
+    })
 })
